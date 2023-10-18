c20cde2c84c6f1d4909fae765a41dc3f07ade19d
KT <thun.kristian@gmail.com>
2023-10-18 11:06:41 +0200

    Added to make bets

--- src/BlackJackPlayer.java
+++ src/BlackJackPlayer.java
@@ -1,12 +1,33 @@
 public class BlackJackPlayer extends Player{
-    int money;
-    int bet;
+    private int money;
+    private int bet;
 
     public BlackJackPlayer(String name, int money){
         super(name);
         this.money = money;
 
     }
-    
+    public int getMoney() {
+        return money;
+    }
 
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public boolean setBet(int bet) {
+        if (bet <= money){
+            money -= bet;
+            this.bet = bet;
+            return true;
+        }
+        return false;
+    }
+    public String toString(){
+        return "Name: " + getName() + " Money " + money + " Bet: " + bet + " Cards: " + getCardsInHand();
+    }
 }
