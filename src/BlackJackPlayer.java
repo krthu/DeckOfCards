db4998d7393126c77309ce97ca48c1125c65a651
KT <thun.kristian@gmail.com>
2023-10-18 10:52:57 +0200

    started on BJPlayer

---
+++ src/BlackJackPlayer.java
@@ -0,0 +1,12 @@
+public class BlackJackPlayer extends Player{
+    int money;
+    int bet;
+
+    public BlackJackPlayer(String name, int money){
+        super(name);
+        this.money = money;
+
+    }
+    
+
+}
